b4667e5057375510ac4c9a633142ad3f78b159a0
Leandro Bittencourt <leandro.dias.bittencourt@gmail.com>
2015-09-01 23:05:02 -0300

    Add Iron Router e template dinamico
    Adicionado template dinamico usando Iron Router

---
+++ client/js/routes/navigation.js
@@ -0,0 +1,18 @@
+Router.route('/', function () {
+   // use the template named ApplicationLayout for our layout
+  this.layout('blogTemplate');
+
+  // render the Post template into the "main" region
+  // {{> yield}}
+  this.render('MainHeader', {to: 'header'});
+
+  
+
+  // render the PostFooter template into the yield region named "footer" 
+  // {{> yield "footer"}}
+  //this.render('PostFooter', {to: 'footer'});
+});
+
+Router.rout('/post', function(){
+    this.render('post');
+});
